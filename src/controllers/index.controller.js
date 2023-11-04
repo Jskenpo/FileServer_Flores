7acc88e748a0ac4b1d73b9fd54297f3a450c93d0
jsantis <san21153@uvg.edu.gt>
2023-11-03 18:34:46 -0600

    endpoint imagenes

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -18,8 +18,6 @@ const getArreglosByCategory = async (req, res) => {
     SELECT
        a.SKU 
     FROM arreglo AS a
-    INNER JOIN categoria AS c ON a.id_categoria = c.id_categoria
-    where a.id_categoria = $1
   `;
     try {
         const response = await pool.query(consulta, [id]);
@@ -46,6 +44,40 @@ const getArreglosByCategory = async (req, res) => {
     }
 };
 
+const getArreglos = async (req, res) => {
+    const consulta = `
+    SELECT
+       a.SKU 
+    FROM arreglo AS a
+  `;
+    try {
+        const response = await pool.query(consulta);
+        const flores = response.rows;
+
+        // Agregar la propiedad 'imagen' a cada restaurante con la ruta del archivo correspondiente
+        const floresConImagen = flores.map(arreglo => ({
+            ...arreglo,
+            imagen: path.join(__dirname, '../imas', `${arreglo.sku}.jpeg`),
+        }));
+
+        // Leer el contenido de cada archivo de imagen y convertirlo a base64
+        const floresConImagenBase64 = await Promise.all(
+            floresConImagen.map(async arreglo => ({
+                ...arreglo,
+                imagen: fs.readFileSync(arreglo.imagen, { encoding: 'base64' }),
+            }))
+        );
+
+        res.status(200).json(floresConImagenBase64);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error en el servidor' });
+    }
+}
+
+
+
 module.exports = {
     getArreglosByCategory,
+    getArreglos
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getArreglosByCategory } = require('../controllers/index.controller');
+const { getArreglosByCategory, getArreglos } = require('../controllers/index.controller');
+
+//GET ALL
+router.get('/arreglos', getArreglos);
 
 //GET BY ID 
 router.get('/arreglos/:id', getArreglosByCategory);
