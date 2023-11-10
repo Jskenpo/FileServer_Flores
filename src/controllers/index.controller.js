2ed6b62bd785743c1d764c4a02035151c0997fc3
jsantis <san21153@uvg.edu.gt>
2023-11-10 00:37:57 -0600

    modificacion a getarreglobyid

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -15,34 +15,36 @@ const pool = new Pool({
 const getArreglosByCategory = async (req, res) => {
     const id = req.params.id;
     const consulta = `
-    SELECT
-       a.SKU 
-    FROM arreglo AS a
-  `;
+      SELECT
+        a.SKU 
+      FROM arreglo AS a
+      WHERE a.id_categoria = $1
+    `;
+  
     try {
-        const response = await pool.query(consulta, [id]);
-        const flores = response.rows;
-
-        // Agregar la propiedad 'imagen' a cada restaurante con la ruta del archivo correspondiente
-        const floresConImagen = flores.map(arreglo => ({
-            ...arreglo,
-            imagen: path.join(__dirname, '../imas', `${arreglo.sku}.jpeg`),
-        }));
-
-        // Leer el contenido de cada archivo de imagen y convertirlo a base64
-        const floresConImagenBase64 = await Promise.all(
-            floresConImagen.map(async arreglo => ({
-                ...arreglo,
-                imagen: fs.readFileSync(arreglo.imagen, { encoding: 'base64' }),
-            }))
-        );
-
-        res.status(200).json(floresConImagenBase64);
+      const response = await pool.query(consulta, [id]);
+      const flores = response.rows;
+  
+      // Agregar la propiedad 'imagen' a cada arreglo con la ruta del archivo correspondiente
+      const floresConImagen = flores.map(arreglo => ({
+        ...arreglo,
+        imagen: path.join(__dirname, '../imas', `${arreglo.sku}.jpeg`),
+      }));
+  
+      // Leer el contenido de cada archivo de imagen y convertirlo a base64
+      const floresConImagenBase64 = await Promise.all(
+        floresConImagen.map(async arreglo => ({
+          ...arreglo,
+          imagen: fs.readFileSync(arreglo.imagen, { encoding: 'base64' }),
+        }))
+      );
+  
+      res.status(200).json(floresConImagenBase64);
     } catch (error) {
-        console.log(error);
-        res.status(500).json({ error: 'Error en el servidor' });
+      console.error("Error en getArreglosByCategory:", error);
+      res.status(500).json({ error: 'Error en el servidor' });
     }
-};
+  };
 
 const getArreglos = async (req, res) => {
     const consulta = `
